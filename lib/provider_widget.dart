c591a5a9207fb0c9e57c7f0db1c806bf3d9fd8bf
Ryan <ryan.c.edge@outlook.com>
2019-12-09 13:45:17 -0500

    Added listen configuration to provider widget
    - Updated HomeViewTraditional example

--- lib/provider_widget.dart
+++ lib/provider_widget.dart
@@ -3,6 +3,10 @@ import 'package:provider/provider.dart';
 
 /// A widget that provides a value passed through a provider as a parameter of the build function.
 abstract class ProviderWidget<T> extends Widget {
+  final bool listen;
+
+  ProviderWidget({Key key, this.listen = true}) : super(key: key);
+
   @protected
   Widget build(BuildContext context, T model);
 
@@ -17,7 +21,8 @@ class _DataProviderElement<T> extends ComponentElement {
   ProviderWidget get widget => super.widget;
 
   @override
-  Widget build() => widget.build(this, Provider.of<T>(this));
+  Widget build() =>
+      widget.build(this, Provider.of<T>(this, listen: widget.listen));
 
   @override
   void update(ProviderWidget newWidget) {
